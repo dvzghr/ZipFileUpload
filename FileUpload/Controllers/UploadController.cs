d18b2850009e39324ef85414e5a6c0585be51dee
thshr <ths_hr@hotmail.com>
2019-10-04 17:50:43 +0200

    Add project files.

---
+++ FileUpload/Controllers/UploadController.cs
@@ -0,0 +1,24 @@
+﻿using System.IO.Compression;
+using Microsoft.AspNetCore.Mvc;
+using static System.String;
+
+namespace FileUpload.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost]
+        public void Post()
+        {
+            var file = Request.Form.Files.GetFile("some_name");
+            var name = Empty;
+
+            using (var zip = new ZipArchive(file.OpenReadStream(), ZipArchiveMode.Read))
+                foreach (var entry in zip.Entries)
+                    name = entry.Name;
+
+            Ok(name);
+        }
+    }
+}
---
+++ SendFile/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using RestSharp;
+
+namespace SendFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendStream();
+        }
+        private static void SendStream()
+        {
+            while (true)
+            {
+                Console.WriteLine("Press to upload...");
+                Console.ReadKey();
+                var client = new RestClient("http://localhost:8080/api");
+                var req = new RestRequest("upload", Method.POST);
+
+                using (var stream = new MemoryStream())
+                {
+                    using (var zip = new ZipArchive(stream, ZipArchiveMode.Create, true))
+                        zip.CreateEntryFromFile("log.txt", "log.txt");
+
+                    //ReceiveStream(stream);
+
+                    var fileParameter = FileParameter.Create("some_name", stream.ToArray(), "upload.zip", "application/zip");
+                    req.Files.Add(fileParameter);
+                    var resp = client.Post(req);
+                    Console.WriteLine(resp.Content);
+                    
+                    Console.WriteLine("Upload finished!");
+                }
+            }
+        }
+
+        private static void ReceiveStream(Stream stream)
+        {
+            using (var zip = new ZipArchive(stream, ZipArchiveMode.Read))
+            {
+                foreach (var entry in zip.Entries)
+                {
+                    Console.WriteLine(entry.Name);
+                }
+            }
+        }
+    }
+}
